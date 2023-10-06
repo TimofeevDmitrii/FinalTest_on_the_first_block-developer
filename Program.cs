157602661d5179022809df5d1fda352690a60679
Dmitry <dimatimoha2014@gmail.com>
2023-10-06 19:05:30 +0300

    Program.cs was added; two main methods in program were wrote - SelectRowsByLength() and PrintStringArray

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿string[] SelectRowsByLength(string[] inletArray, int selectLength)
+{
+    int i = 0;
+    int k = 0;
+    while (i<inletArray.Length)
+    {
+        if (inletArray[i].Length<=selectLength)
+            k++;
+        i++;
+    }
+    string[] outletArray = new string[k];
+    i = 0;
+    int n = 0;
+    while (i<inletArray.Length)
+    {
+        if (inletArray[i].Length<=selectLength)
+        {
+            outletArray[n] = inletArray[i];
+            n++;
+        }
+        i++;
+    }
+    return outletArray;
+}
+void PrintStringArray(string[] array)
+{
+    int l = 0;
+    System.Console.Write("[");
+    for (l=0; l<array.Length; l++)
+    {
+        System.Console.Write($"\"{array[l]}\"");
+        if (l!=array.Length-1)
+            System.Console.Write(", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
